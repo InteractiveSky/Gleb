a15bfb4ad850bfb77545a628a2988aef6ab36fd7
neokortex <neokortex@mail.ru>
2014-07-12 23:31:37 +0400

    Добавил site_closed.php

---
+++ bitrix/php_interface/include/site_closed.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+    <!--[if lt IE 9]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <title>Site under construction</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/b.css">
+    <link rel="stylesheet" href="/bitrix/templates/template_name/template_styles.css">
+</head>
+<body>
+
+<p>Site under construction</p>
+
+<script src="http://yandex.st/jquery/2.1.1/jquery.min.js"></script>
+<script src="/js/vendor/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="/js/b.js"></script>
+<script src="/js/scripts.js"></script>
+</body>
+</html>
