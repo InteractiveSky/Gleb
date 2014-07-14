0e0d4677bae33b5ab075e7cfd62c141608aad580
Averichev Alexander <neokortex@mail.ru>
2014-07-14 15:01:07 +0400

    Добавил jquery.colorizeimage.js и документацию к нему

---
+++ js/vendor/jquery.colorizeimage/js/jquery.colorize-image.js
@@ -0,0 +1,33 @@
+(function ($) {
+    $.fn.ColorizeImage = function () {
+        var obj = $(this);
+        var selector = obj.selector;
+        var n_class = selector.substring(1);
+        var wrapper = $('<div class="' + n_class + '"></div>').css({display: 'inline-block', position: 'relative'});
+        obj.each(function (i, o) {
+            var img = $(o);
+            img.css({position: 'relative', zIndex: 1});
+            var colored = img.attr('data-colored');
+            if (colored !== undefined) {
+                var col_img = $('<img alt="" src="' + colored + '" />').css({
+                    display: 'block',
+                    position: 'absolute',
+                    zIndex: 2,
+                    top:0,
+                    left:0
+                });
+                col_img.addClass(n_class + '__colored').hide();
+                img.addClass(n_class + '__uncolored').removeClass(n_class).wrap(wrapper).after(col_img);
+            }
+        });
+        $('body').on('mouseenter', selector + '__uncolored', function () {
+            var cl = $(this).closest(selector);
+            var colored = cl.find(selector + '__colored');
+            //console.log(colored);
+            colored.stop(true, true).fadeIn(300);
+        });
+        $('body').on('mouseleave', selector + '__colored', function () {
+            $(this).stop(true, true).fadeOut(300);
+        });
+    };
+})(jQuery);
