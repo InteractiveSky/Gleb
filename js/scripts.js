5d2fd17d72da3b3e42b089dee96ad7a9fb175e49
Averichev Alexander <neokortex@mail.ru>
2014-07-16 16:54:44 +0400

    Исправил ссылку на css фенсибокса. Добавил опции к datepicker

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.bxslider').bxSlider({
         pager: false
     });
 });
-$(window).load(function(){
+$(window).load(function () {
 
 });
-
 $(window).scroll(function () {
 
 });
-
-function datePicker(){
-    $( ".input-text--date" ).datepicker({
+function datePicker() {
+    $(".input-text--date").datepicker({
         showOn: "both",
-        minDate:0,
+        minDate: 0, // возможность выбрать начиная с текущей даты
         buttonImage: false,
-        buttonImageOnly: false
+        buttonImageOnly: false,
+        showButtonPanel: true,
+        buttonText: "Выбрать дату"
     });
 }
