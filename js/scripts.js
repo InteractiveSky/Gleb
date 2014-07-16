f0bdaaf618248c2840e84d8ac0049e9c8e495a4d
Averichev Alexander <neokortex@mail.ru>
2014-07-16 16:31:21 +0400

    Добавил заготовку для jQuery UI datepicker

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,11 @@ $(window).scroll(function () {
 
 });
 
+function datePicker(){
+    $( ".input-text--date" ).datepicker({
+        showOn: "both",
+        minDate:0,
+        buttonImage: false,
+        buttonImageOnly: false
+    });
+}
