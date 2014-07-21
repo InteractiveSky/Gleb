827b4f78372a00360818e6273111ceee6ee6ab9f
Averichev Alexander <neokortex@mail.ru>
2014-07-21 11:11:55 +0400

    Добавлен timepicker в js

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,38 @@ $(window).load(function () {
 $(window).scroll(function () {
 
 });
-function bxSlider(){
+function timepicker() {
+    $.timepicker.regional['ru'] = {
+        timeOnlyTitle: 'Выберите время',
+        timeText: 'Время',
+        hourText: 'Часы',
+        minuteText: 'Минуты',
+        secondText: 'Секунды',
+        millisecText: 'Миллисекунды',
+        timezoneText: 'Часовой пояс',
+        currentText: 'Сейчас',
+        closeText: 'Ok',
+        timeFormat: 'HH:mm',
+        amNames: ['AM', 'A'],
+        pmNames: ['PM', 'P'],
+        isRTL: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['ru']);
+    var s = 'input[data-type="time"]';
+    var input_time = $(s);
+    input_time.after('<button type="button" class="ui-datepicker-trigger ui-datepicker-trigger--time">Выбрать время</button>');
+    $('body').on('click', '.ui-datepicker-trigger--time', function (e) {
+        e.preventDefault();
+        $(this).prev().focus();
+    });
+    input_time.timepicker({stepMinute: 5});
+}
+function bxSlider() {
     $('.bxslider').bxSlider({
         pager: false
     });
 }
-function ui_tabs(){
+function ui_tabs() {
     $(".tabs").tabs();
 }
 function datePicker() {
@@ -28,7 +54,7 @@ function datePicker() {
 function google_maps() {
     var map_div = $('[data-map="google"]');
     if (map_div.size() > 0) {
-        map_div.each(function(i, o){
+        map_div.each(function (i, o) {
             var center_coords_attr = $(o).attr('data-coords');
             var center_coords = center_coords_attr.split(/[\s,]+/);
             var mapOptions = {
@@ -37,7 +63,7 @@ function google_maps() {
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 disableDefaultUI: true,
                 panControl: false,
-                streetViewControl:true,
+                streetViewControl: true,
                 zoomControl: true,
                 scaleControl: false,
                 mapTypeControl: false,
@@ -51,7 +77,7 @@ function google_maps() {
                 new google.maps.Point(25, 25));
             var map = new google.maps.Map(o, mapOptions);
             var object_coords_attr = $(o).attr('data-object');
-            if (object_coords_attr!==undefined) {
+            if (object_coords_attr !== undefined) {
                 var object_coords = object_coords_attr.split(/[\s,]+/);
                 var objectCoords = new google.maps.LatLng(object_coords[0], object_coords[1]);
                 var objectMarker = new google.maps.Marker({
