953a80c764579a299bb608a6eb3ae626cb745b80
Averichev Alexander <neokortex@mail.ru>
2014-07-25 11:25:24 +0400

    Исправил ошибку

--- js/vendor/jquery.colorizeimage/js/jquery.colorize-image.js
+++ js/vendor/jquery.colorizeimage/js/jquery.colorize-image.js
@@ -7,27 +7,29 @@
         obj.each(function (i, o) {
             var img = $(o);
             var colored = img.attr('data-colored');
-            console.log(colored);
             if (colored !== undefined && colored!=='') {
                 var col_img = $('<img alt="" src="' + colored + '" />').css({
                     display: 'block',
                     position: 'absolute',
                     zIndex: 2,
                     top:0,
-                    left:0
+                    left:0,
+                    opacity:0
                 });
-                col_img.addClass(n_class + '__colored').hide();
+                col_img.addClass(n_class + '__colored');
             }
             img.addClass(n_class + '__uncolored').removeClass(n_class).wrap(wrapper).after(col_img);
         });
         var body = $('body');
-        body.on('mouseenter', selector + '__uncolored', function () {
-            var cl = $(this).closest(selector);
-            var colored = cl.find(selector + '__colored');
-            colored.stop(true, true).fadeIn(300);
+        body.on('mouseenter', selector + '__colored', function () {
+            $(this).stop(true, true).animate({
+                opacity: 1
+            }, 300)
         });
         body.on('mouseleave', selector + '__colored', function () {
-            $(this).stop(true, true).fadeOut(300);
+            $(this).stop(true, true).animate({
+                opacity: 0
+            }, 300)
         });
     };
 })(jQuery);
