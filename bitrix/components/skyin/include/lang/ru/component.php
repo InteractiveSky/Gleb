ca67fc957962663d2a525236add17b5978c6e58f
spleaner <spleaner.ru@gmail.com>
2014-08-05 10:43:34 +0400

    Добавил свой компонент включаемой области. Отличается от стандартного тем, что есть параметр «Название» и не будет путаницы, если их несколько в одном месте. Везде будут человеческие надписи (например "Редактировать «Контакты»", "Добавить «Телефоны»" и т.п.)

---
+++ bitrix/components/skyin/include/lang/ru/component.php
@@ -0,0 +1,20 @@
+<?
+$MESS["MAIN_INCLUDE_AREA_EDIT_page"] = "Редактировать #NAME# текущей страницы";
+$MESS["MAIN_INCLUDE_AREA_EDIT_sect"] = "Редактировать #NAME# раздела";
+$MESS["MAIN_INCLUDE_AREA_EDIT_file"] = "Редактировать #NAME#";
+$MESS["MAIN_INCLUDE_AREA_ADD_page"] = "Добавить #NAME# для текущей страницы";
+$MESS["MAIN_INCLUDE_AREA_ADD_sect"] = "Добавить #NAME# для текущего раздела";
+$MESS["MAIN_INCLUDE_AREA_ADD_file"] = "Создать #NAME#";
+$MESS["MAIN_INCLUDE_AREA_EDIT_page_NOEDITOR"] = "Редактировать #NAME# текущей страницы как PHP";
+$MESS["MAIN_INCLUDE_AREA_EDIT_sect_NOEDITOR"] = "Редактировать #NAME# раздела как PHP";
+$MESS["MAIN_INCLUDE_AREA_EDIT_file_NOEDITOR"] = "Редактировать #NAME# как PHP";
+$MESS["MAIN_INCLUDE_AREA_ADD_page_NOEDITOR"] = "Добавить #NAME# для текущей страницы как PHP";
+$MESS["MAIN_INCLUDE_AREA_ADD_sect_NOEDITOR"] = "Добавить #NAME# для текущего раздела как PHP";
+$MESS["MAIN_INCLUDE_AREA_ADD_file_NOEDITOR"] = "Создать #NAME# как PHP";
+$MESS["main_comp_include_edit"] = "Изменить #NAME#";
+$MESS["main_comp_include_edit_php"] = "Изменить #NAME# как PHP";
+$MESS["main_comp_include_add"] = "Добавить";
+$MESS["main_comp_include_add_php"] = "Добавить как PHP";
+$MESS["main_comp_include_add1"] = "Добавить #NAME#";
+$MESS["main_comp_include_add1_php"] = "Добавить #NAME# как PHP";
+?>
