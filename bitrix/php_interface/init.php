6f768edace059bba7a7f9036a3495f52f3d7ec44
spleaner <spleaner.ru@gmail.com>
2014-08-13 13:00:11 +0400

    Добавил обработчик и инструкцию для уведомлений о регистрации (в том числе скриптами и при покупке)

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -1,3 +1,37 @@
 <?php
 
 require_once 'Gleb.php';
+
+/*
+Отправляем пользователю логин/пароль при любой регистрации (и настроящей, и скриптом, и при заказе)
+Для того, чтобы отправлялось это уведомление нужно:
+1. деактивировать шаблон по событию "NEW_USER"
+2. создать почтовое событие SKYIN_NEW_USER
+3. шаблон для него примерно следующего характера:
+	Здравствуйте!
+
+	Для Вас был создан аккаунт:
+
+	Логин: #LOGIN#
+	Пароль: #PASSWORD#
+*/
+AddEventHandler("main", "OnAfterUserAdd", "OnAfterUserRegisterHandler");
+AddEventHandler("main", "OnAfterUserRegister", "OnAfterUserRegisterHandler");
+AddEventHandler("main", "OnAfterUserSimpleRegister", "OnAfterUserRegisterHandler");
+function OnAfterUserRegisterHandler(&$arFields)
+{
+    if (intval($arFields["ID"])>0)
+    {
+        $toSend = Array();
+        $toSend["PASSWORD"] = $arFields["CONFIRM_PASSWORD"];
+        $toSend["EMAIL"] = $arFields["EMAIL"];
+        $toSend["USER_ID"] = $arFields["ID"];
+        $toSend["USER_IP"] = $arFields["USER_IP"];
+        $toSend["USER_HOST"] = $arFields["USER_HOST"];
+        $toSend["LOGIN"] = $arFields["LOGIN"];
+        $toSend["NAME"] = trim($arFields["NAME"]);
+        $toSend["LAST_NAME"] = trim ($arFields["LAST_NAME"]);
+        CEvent::SendImmediate("SKYIN_NEW_USER", SITE_ID, $toSend);
+    }
+    return $arFields;
+}
